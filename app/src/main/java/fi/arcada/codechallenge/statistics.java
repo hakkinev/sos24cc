2dda6c3e2f5bad5a9e4288825e10c71469aa4650
hakkinev <vinski.hakkinen@gmail.com>
2024-04-05 19:50:09 +0300

    Code Challenge: The meaning of mean

--- app/src/main/java/fi/arcada/codechallenge/statistics.java
+++ app/src/main/java/fi/arcada/codechallenge/statistics.java
@@ -1,12 +1,48 @@
 package fi.arcada.codechallenge;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
 public class statistics {
 
-    public static double calcMean(double[] values) {
-        double sum = 0;
-        for (int i = 0; i < values.length; i++) {
-            sum += values[i];
-        }
-        return sum / values.length;
+    public static double calcMedian(ArrayList<Double> values) {
+
+        // Vi skapar en kopia av values
+        ArrayList<Double> sorted = new ArrayList<>(values);
+
+        // Sedan sorterar vi kopian. Annars sorteras ursprungliga datanmängden
+        Collections.sort(sorted);
+
+        // Detta funkar, ännu bättre skulle vara att kolla skiljt för jämna datamängder -
+        // - med medelvärde av de två mittersta
+        int midIndex = sorted.size() / 2;
+
+        // Medianen är dett mittersta värdet i vår sorterade datamängd!
+        return sorted.get(midIndex);
+
     }
+
+    public static double calcMean(ArrayList<Double> values) {
+        double sum = 0;
+        for (int i = 0; i < values.size(); i++) {
+            sum += values.get(i);
+        }
+        return sum / values.size();
+    }
+
+    public static double calcStdev(ArrayList<Double> values) {
+        // Räkna medelvärdet av de givna värdena
+        double mean = calcMean(values);
+
+        // Beräkna summan av kvadraten på skillnaden mellan varje värde och medelvärdet
+        double avvikelse = 0;
+        for (int i = 0; i < values.size(); i++) {
+            avvikelse += Math.pow(values.get(i) - mean, 2);
+        }
+
+        // Beräkna kvadratroten ur den genomsnittliga avvikelsen för att få standardavvikelsen
+        return Math.sqrt(avvikelse / values.size());
+    }
+
 }
