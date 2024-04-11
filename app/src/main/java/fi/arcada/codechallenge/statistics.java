e09a83e4e08b582b47c72442b9a0c605c0b8acb2
hakkinev <vinski.hakkinen@gmail.com>
2024-04-11 19:34:29 +0300

    CC - Kvartiler

--- app/src/main/java/fi/arcada/codechallenge/statistics.java
+++ app/src/main/java/fi/arcada/codechallenge/statistics.java
@@ -6,13 +6,18 @@ import java.util.Collections;
 
 public class statistics {
 
-    public static double calcMedian(ArrayList<Double> values) {
-
+    public static ArrayList<Double> getSorted(ArrayList<Double> values) {
         // Vi skapar en kopia av values
         ArrayList<Double> sorted = new ArrayList<>(values);
 
-        // Sedan sorterar vi kopian. Annars sorteras ursprungliga datanmängden
+        // Vi sorterar kopiam, annars sorteras ursprungliga datamängden
         Collections.sort(sorted);
+        return sorted;
+    }
+
+    public static double calcMedian(ArrayList<Double> values) {
+
+        ArrayList<Double> sorted = getSorted(values);
 
         // Detta funkar, ännu bättre skulle vara att kolla skiljt för jämna datamängder -
         // - med medelvärde av de två mittersta
@@ -44,5 +49,23 @@ public class statistics {
         // Räkna kvadratroten ur den genomsnittliga avvikelsen för att få standardavvikelsen!
         return Math.sqrt(avvikelse / values.size());
     }
+
+    public static double calcLQ(ArrayList<Double> values){
+        ArrayList<Double> sorted = getSorted(values);
+        int lQIndex = sorted.size() / 4;
+        return sorted.get(lQIndex);
+    }
+    public static double calcUQ(ArrayList<Double> values) {
+        ArrayList<Double> sorted = getSorted(values);
+        int uQIndex = sorted.size() * 3 / 4; //
+        return sorted.get(uQIndex);
+    }
+
+    public static double calcIQR(ArrayList<Double> values) {
+        double lq = calcLQ(values);
+        double uq = calcUQ(values);
+        return uq - lq; // Interkvartilavståndet är skillnaden mellan övre och undre kvartilen
+    }
+
 
 }
